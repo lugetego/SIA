41349adf380045c6671c269ac8e001020e0ee194
Miguel Magaña <miguel@matmor.unam.mx>
2014-10-29 10:35:58 -0600

    Script Address.js

--- src/Ccm/SiaBundle/Resources/public/js/address.js
+++ src/Ccm/SiaBundle/Resources/public/js/address.js
@@ -1,16 +1,11 @@
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
-<script>
 // This example displays an address form, using the autocomplete feature
 // of the Google Places API to help users fill in the information.
 
 var placeSearch, autocomplete;
 var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
     locality: 'long_name',
     administrative_area_level_1: 'short_name',
-    country: 'long_name',
-    postal_code: 'short_name'
+    country: 'long_name'
 };
 
 function initialize() {
@@ -62,5 +57,3 @@ function geolocate() {
     }
 }
 // [END region_geolocation]
-
-</script>
